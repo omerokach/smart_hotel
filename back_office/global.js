c89a9f16f9a255df087c717b35b55deb39d4d310
yarden123huberman-tech <yarden123huberman@gmail.com>
2025-11-20 19:48:31 +0200

    Delete back_office/global.js

--- back_office/global.js
+++
@@ -1,7 +0,0 @@
-function showLoader() {
-  document.getElementById("global-loader").classList.remove("hidden");
-}
-
-function hideLoader() {
-  document.getElementById("global-loader").classList.add("hidden");
-}
